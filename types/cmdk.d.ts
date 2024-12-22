ee7112c5b8b3bb294bef9bcdb9c11bf6f9006227
Dogosele <mwapodzoh6@@gmail.com>
2024-12-23 01:36:44 +0300

    feat: add command component with CMDK integration
    - Add CMDK type definitions
    - Update command component implementation
    - Configure TypeScript for CMDK support
    - Update project dependencies

---
+++ types/cmdk.d.ts
@@ -0,0 +1,9 @@
+declare module 'cmdk' {
+  export const Command: any;
+  export const Input: any;
+  export const List: any;
+  export const Empty: any;
+  export const Group: any;
+  export const Separator: any;
+  export const Item: any;
+}
